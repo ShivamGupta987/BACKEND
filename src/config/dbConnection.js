a078555e78210238a0d3ce539d9b9780817f8828
ShivamGupta987 <118923085+ShivamGupta987@users.noreply.github.com>
2024-02-21 16:48:42 +0530

    none cahnges

--- src/config/dbConnection.js
+++ src/config/dbConnection.js
@@ -1,26 +1,18 @@
-import mongoose, { connect }  from "mongoose";
+import mongoose, { connect } from "mongoose";
 
-mongoose.set('strictQuery',false);
+mongoose.set("strictQuery", false);
 
-const connectionToDB = async() =>{
-    try{
-        const {connection} = await mongoose.connect(process.env.MONGO_URI
-            );
-    
-        if(connection) {
-            console.log(`connectioned DB : ${connection.host}`)
-        }
-    
-        
-    } catch(e){
-        console.log(e);
-        process.exit(1);
+const connectionToDB = async () => {
+  try {
+    const { connection } = await mongoose.connect(process.env.MONGO_URI);
+
+    if (connection) {
+      console.log(`connectioned DB : ${connection.host}`);
     }
-  
-
-}
-
-
-
+  } catch (e) {
+    console.log(e);
+    process.exit(1);
+  }
+};
 
 export default connectionToDB;
