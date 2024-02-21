140ec8713571e5207607cf162f13bc1b6747a1db
ShivamGupta987 <118923085+ShivamGupta987@users.noreply.github.com>
2024-02-21 16:51:10 +0530

    error handling

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -5,9 +5,11 @@ import jwt from "jsonwebtoken";
 const isLoggedIn = async (req,res,next) =>{
     const {token} = req.cookies;
 
-    if(!token){
-        return new(AppError('token not find',400))
-    }
+        if (!token) {
+            return next(new AppError('Token not found', 400));
+        }
+        
+    
 
     const userDetails = await jwt.verify(token,process.env.JWT_SECRET);
     req.user = userDetails;
